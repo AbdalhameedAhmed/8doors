7ef31b33fa75c5d66384877ae3d47a0145476135
Abdalhameed-Ahmed <abdalhameed.ahmed15@gmail.com>
2023-03-11 01:26:28 +0200

    use react final form in login page

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,23 +1,19 @@
-import React, { useState } from 'react';
-import Link from 'next/link';
-import { useTranslation } from 'react-i18next';
-import { useRouter } from 'next/router';
-import { useForm, Controller } from 'react-hook-form';
-import { useDispatch, useSelector } from 'react-redux';
-import Logo from '../../assets/logo.svg';
-import { CustomInput } from 'components/shared';
-import { login } from 'api'
+import React, { useState } from "react";
+import Link from "next/link";
+import { useTranslation } from "react-i18next";
+import { useRouter } from "next/router";
+import { Form, Field } from "react-final-form";
+import { useDispatch, useSelector } from "react-redux";
+import Logo from "../../assets/logo.svg";
+import { CustomInput } from "components/shared";
+import { login } from "api";
 import { useGetPetQuery, api } from "redux/services/clinic/auth";
-
+import { isMetaProperty } from "typescript";
 
 function SignIn() {
-  const { t } = useTranslation('common');
-  const {
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-  const [error, setError] = useState('');
+  const { t } = useTranslation("common");
+
+  const [error, setError] = useState("");
   const dispatch = useDispatch();
   const router = useRouter();
   console.log(router.pathname);
@@ -30,80 +26,99 @@ function SignIn() {
 
   const [trigger, { data }] = api.useLazyGetPetQuery();
 
-  const onSubmit = async (data: any) => {
-    const res = await login(data)
-    if (res) {
-      await trigger(true)
-      console.log(data)
-      router.push('/');
-    } else {
-      setError('User is not registered yet!!');
-      setTimeout(() => {
-        setError('');
-      }, 3000);
-    }
+  const onSubmit = async (values: any) => {
+    // const res = await login(data)
+    // if (res) {
+    //   await trigger(true)
+    //   console.log(data)
+    //   router.push('/');
+    // } else {
+    //   setError('User is not registered yet!!');
+    //   setTimeout(() => {
+    //     setError('');
+    //   }, 3000);
+    // }
   };
 
   return (
-    <div className={`self-center px-10 py-10 w-96`}>
-      <div className="flex  align-center justify-center">
-        <Logo style={{ height: 65, width: 65 }} />
+    <>
+      <div className={`self-center px-10 py-10 w-96`}>
+        <div className="flex  align-center justify-center">
+          <Logo style={{ height: 65, width: 65 }} />
+        </div>
+        <h1 className="text-center text-3xl my-2 text-white">
+          {" "}
+          {t("signin.login")}
+        </h1>
+
+        <Form
+          onSubmit={onSubmit}
+          validate={(values) => {
+            const errors: any = {};
+
+            if (!values.userName) {
+              errors.userName = "Required";
+            }
+            if (!values.passWord) {
+              errors.passWord = "Required";
+            }
+            return errors;
+          }}
+          render={({ handleSubmit, submitting, form, pristine, values }) => (
+            <form onSubmit={handleSubmit}>
+              <Field name="userName">
+                {({ input, meta }) => (
+                  <>
+                    <CustomInput
+                      label=""
+                      placeholder={t("signup.email")}
+                      className={`signin-signout-input w-full rounded-lg`}
+                      type="text"
+                      {...input}
+                    />
+                    {meta.error && meta.touched && (
+                      <p className="text-red-500 text-center text-sm mt-2">
+                        {meta.error}
+                      </p>
+                    )}
+                  </>
+                )}
+              </Field>
+              <Field name="passWord">
+                {({ input, meta }) => (
+                  <>
+                    <CustomInput
+                      placeholder={t("signin.password")}
+                      type="password"
+                      className={`signin-signout-input w-full rounded-lg`}
+                      {...input}
+                    />
+                    {meta.error && meta.touched && (
+                      <p className="text-red-500 text-center text-sm mt-2">
+                        {meta.error}
+                      </p>
+                    )}
+                  </>
+                )}
+              </Field>
+              <button
+                type="submit"
+                className="mt-5 w-full rounded-lg text-white p-4 bg-sky-500/100"
+                disabled={submitting}
+              >
+                {t("signin.login")}
+              </button>
+
+              <p className="mt-3 text-sm text-white text-center">
+                <Link href="/forget-password">
+                  <u>{" " + t("signin.forgetPassword")}</u>
+                </Link>
+              </p>
+            </form>
+          )}
+        ></Form>
       </div>
-      <h1 className="text-center text-3xl my-2 text-white"> {t('signin.login')}</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-
-        <Controller
-          control={control}
-          rules={{
-            required: true,
-          }}
-          render={({ field: { onChange, onBlur, value } }) => (
-            <CustomInput
-              label=""
-              placeholder={t('signup.email')}
-              error={errors?.email ? t('signin.emailIsRequired') : ''}
-              value={value}
-              onChange={onChange}
-              onBlur={onBlur}
-              className={`signin-signout-input w-full rounded-lg ${errors?.email ? 'border-red-500' : ''
-                }`}
-              type="text"
-            />
-          )}
-          name="username"
-        />
-
-        <Controller
-          control={control}
-          rules={{
-            required: true,
-          }}
-          render={({ field: { onChange, onBlur, value } }) => (
-            <CustomInput
-              placeholder={t('signin.password')}
-              type="password"
-              value={value}
-              onChange={onChange}
-              error={errors?.password ? t('signin.passwordIsRequired') : ''}
-              onBlur={onBlur}
-              className={`signin-signout-input w-full rounded-lg ${errors?.password ? 'border-red-500' : ''
-                }`}
-            />
-          )}
-          name="password"
-        />
-
-        {error && <p className="text-red-500 text-center text-sm mt-2">{error}</p>}
-        <button type="submit" className="mt-5 w-full rounded-lg text-white p-4 bg-sky-500/100">
-          {t('signin.login')}
-        </button>
-        <p className="mt-3 text-sm text-white text-center">
-          <Link href="/forget-password">
-            <u>{' ' + t('signin.forgetPassword')}</u>
-          </Link>
-        </p>
-      </form>
-    </div>
+    </>
   );
 }
 
